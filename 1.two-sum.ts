cb18984349776e51e4caaae4dd4fd81522759627
Nigel Dsouza <nigel10122@gmail.com>
2024-01-21 16:22:59 -0600

    Git init leetcode

---
+++ 1.two-sum.ts
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+    
+    let arr : number[] = []
+
+
+
+    return arr;
+};
+// @lc code=end
+
